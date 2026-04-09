90d6b8a7e735ef2974e49bd46a174ac0b94b0f24
Viet <78581342+viet456@users.noreply.github.com>
2026-04-08 23:21:02 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left <= right) {
+            if (s[left] !== s[right]) {
+                return false;
+            }
+            left++
+            right--
+        }
+        return true
+    }
+}
