5f2e82719ac89f8f6c560ec562223bb2d93f76ff
Viet <78581342+viet456@users.noreply.github.com>
2026-04-11 23:47:29 -0700

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let min = Infinity;
+        let max = 0;
+        for (let i=0; i<prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i]
+            }
+            if (prices[i] - min > max) {
+                max=prices[i] - min;
+            }
+        }
+
+        return max;
+    }
+}
