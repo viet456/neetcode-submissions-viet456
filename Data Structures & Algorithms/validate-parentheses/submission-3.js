15391a5ea4ba244e588cbfebc70883b43f921e5e
Viet <78581342+viet456@users.noreply.github.com>
2026-04-12 20:47:50 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        let map = {
+            '(' :')',
+            '[':']',
+            '{':'}'
+        }
+        for (let char of s) {
+            if (map[char]) stack.push(char)
+            else {
+                let lastOpener = stack.pop();
+                if (map[lastOpener] !== char) return false
+            }
+        }
+        return stack.length === 0
+    }
+}
