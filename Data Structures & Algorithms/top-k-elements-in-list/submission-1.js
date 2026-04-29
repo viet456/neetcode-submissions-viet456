17cc3272a141df1e01796cbb270087b06990769c
Viet <78581342+viet456@users.noreply.github.com>
2026-04-29 00:23:54 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let freq = new Map();
+        for (let i = 0; i < nums.length; i++) {
+            // freq:num
+            freq.set(nums[i], (freq.get(nums[i]) || 0 )+ 1);
+        };
+
+        let buckets = new Array(nums.length + 1).fill(0).map(() => []);
+        for (let [num, f] of freq) {
+            buckets[f].push(num)
+        }
+        let right = buckets.length-1;
+        let ans = [];
+        while (ans.length < k) {
+            if (buckets[right].length > 0) {
+                ans.push(...buckets[right])
+            }
+            right--
+        }
+        return ans
+    }
+}
